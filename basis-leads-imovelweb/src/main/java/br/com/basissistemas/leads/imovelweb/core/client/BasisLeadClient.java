dfdca4009eb8f7d2231bc83ca89d105e74e6b138
Alexandre <alexandre@basissistemas.com.br>
2022-10-16 12:04:55 -0300

    application.properties - adicionado o usuario e senha do basis-leads-api BasisLeadClient - alterado para pegar o usuario e senha do application.properties.

--- basis-leads-imovelweb/src/main/java/br/com/basissistemas/leads/imovelweb/core/client/BasisLeadClient.java
+++ basis-leads-imovelweb/src/main/java/br/com/basissistemas/leads/imovelweb/core/client/BasisLeadClient.java
@@ -1,5 +1,8 @@
 package br.com.basissistemas.leads.imovelweb.core.client;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,14 +20,24 @@ public class BasisLeadClient {
 	@Value("${basis.lead.api.porta}")
 	private String portaApi;
 	
+	@Value("${basis.lead.api.username}")
+	private String username;
+	
+	@Value("${basis.lead.api.password}")
+	private String password;
+	
 	@Bean("leadClient")
 	public WebClient webClientBasisLead(WebClient.Builder builder) {
-		String basicToken = "Basic YmFzaXNfbGVhZHM6JDJhJDEyJGo4RFhoZ0liNU12ZnhGZ01MZGd5Vy5FRUguQmZVR0Ryb0Z5VUZWN2FNeGZFVy5nbVVRcXhx";
+		
+		String auth = username + ":" + password;
+        String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String( encodedAuth );
+        
 		String urlApi = protocoloApi.concat("://").concat(hostApi).concat(":").concat(portaApi);
 		
 		return builder
 				.baseUrl(urlApi)
-				.defaultHeader("Authorization", basicToken)
+				.defaultHeader("Authorization", authHeader)
 				.build();
 	}
 	
